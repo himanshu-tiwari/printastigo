a1f309fdf8d347e1cc54bc0ccf371b8eb33fb4ef
himanshu tiwari <htiwarih@gmail.com>
2017-06-15 00:05:07 +0530

    added templating for products

---
+++ app/Models/Product.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Printastigo\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+	protected $table = 'products';
+
+// 	protected $fillable = [
+//         'title',
+//         'slug',
+//         'description',
+//         'price',
+//         'image',
+//         'stock'
+// 	];
+}
+
+?>
