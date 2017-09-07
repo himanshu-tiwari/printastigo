675b15e812ef361d273686b20fee8ad7b6807deb
himanshu tiwari <htiwarih@gmail.com>
2017-09-07 22:25:49 +0530

    added Router class to product controller

--- app/Controllers/ProductController.php
+++ app/Controllers/ProductController.php
@@ -2,13 +2,14 @@
 
 namespace Printastigo\Controllers;
 
+use Slim\Router;
 use Slim\Views\Twig;
 use Printastigo\Models\Product;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
 class ProductController{
-	public function get($slug, Request $request, Response $response, Twig $view, Product $product){
+	public function get($slug, Request $request, Response $response, Twig $view, Product $product, Router $router){
 		$product = $product->where('slug', $slug)->first();
 		
 		if (!$product) {
